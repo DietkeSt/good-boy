77906f83f9524649e8808daef22c0ca1bbd9be8e
DietkeSt <>
2023-07-01 18:08:07 +0000

    Add modal for instructions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Effect for image slider on lading page
+// Function for image slider and fade effect on lading page
 
 window.addEventListener("DOMContentLoaded", function (e) {
     var stage = document.getElementById("image-slider");
@@ -23,3 +23,15 @@ window.addEventListener("DOMContentLoaded", function (e) {
     images[currentImageIndex].classList.add("active");
     setTimeout(fadeToNextImage, slideInterval);
 }, false);
+
+// Function to show alert box when clicking on the instructions on the landing page
+function showAlertBox() {
+    var instructions = "This is how the quiz works: 1. Please enter your name. 2. Click on the 'Start Quiz' button. 3. Guess the correct dog breed by clicking on the option with the correct breed name displayed. 4. Click the 'Next' button. 5. Repeat until the Quiz is finished and you see your final score!";
+
+    // Display the instructions in an alert box
+    alert(instructions);
+}
+
+// Add a click event listener to the instructions element
+var instructionsElement = document.querySelector(".instructions-landing");
+instructionsElement.addEventListener("click", showAlertBox);
