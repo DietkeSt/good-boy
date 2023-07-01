8659614e69aa11aaf769edc9845ccbddb588e523
DietkeSt <>
2023-07-01 14:32:48 +0000

    Adjust image slider styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-console.log("connected!");
+// Fading effect for image slider on lading page
+
+const images = document.querySelectorAll("#image-slider img");
+let currentIndex = 0;
+
+function showImage(index) {
+    images.forEach((image, i) => {
+        if (i === index) {
+            image.classList.add("fade-in");
+        } else {
+            image.classList.remove("fade-in");
+        }
+    });
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+}
+
+setInterval(nextImage, 5000); 
